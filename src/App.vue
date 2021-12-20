08fe42a9c94634dccd7b4b3c3d433c43d0a3c787
Jorshua Hernandez <jorshua321@gmail.com>
2021-12-20 15:23:16 -0500

    inicio de ejemplos usando Vue CLI

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,53 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h2>{{titulo}}</h2>
+    <ninjas></ninjas>
   </div>
 </template>
 
+<script>
+  //al querer importar componentes para anidarlos lo podemos hacer de dos formas
+  //global: significa que lo podemos usar en otros componentes y localmente
+
+
+  //Forma local: se importa el componente y se le asigna el nombrea usar por el import
+  import Ninjas from "./Ninjas.vue"
+
+  export default ({
+    //declaramos el objeto components y dentro de el insertamos los componentes que usaremos localmente en esta vista
+    components: {
+      // asignamos el alias con el que se llamara al componente y el nombre de donde viene dicho componente en el import
+      'ninjas': Ninjas
+    },
+    data() {
+      return {
+        titulo: 'Prueba App'
+      }
+    }
+  })
+</script>
+
+/* Si se agrega un estilo personalizado a una etiqueta en el archivo base de la aplicacion 
+esta puede ser sobreescrita por los estilos personalizados del componente que se esta importando
+para evitar esto hacemos uso de la palabra scoped en la etiqueta style -> <style scoped> </style>
+de esta forma le indicamos al DOM que solo aplique los estilos indicados a este elemento y no a los demas */
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #app {
+    font-family: Arial, Helvetica, sans-serif;
+    display: flex;
+    height: 100vh;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  h2{
+    font-size: 28px;
+    color:violet;
+  }
+  
 </style>
+
+components: { Ninjas },
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// import Ninjas from './Ninjas.vue'
+// import Inicio from './Inicio.vue'
+//Al importar un compone
+// Vue.component('ninjas',  Ninjas);
+
+//si queremos importar el componente globalmente lo hacemos desde este archivo, con esto damos la posibilidad de usar el componente en todo el proyecto
 
 Vue.config.productionTip = false
 
