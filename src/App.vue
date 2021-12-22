3939069a6d7ea2f164455c6cedbcb6f71b060046
Jorshua Hernandez <jorshua321@gmail.com>
2021-12-22 15:18:21 -0500

    se agrega comentarios sobre el uso de los componentes diinamicos y archivos necesarios para demo

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,49 @@
 <template>
-  <div id="app">
-  <form-helper>
-    <!-- al agregarle el parametro slot a la etiqueta de texto que estamos insertando en el componente le indicamos al slot del componente que recibe en donde debe posicionar el texto -->
-    <h2 slot="titulo"> {{titulo}} </h2>
-    <p slot="texto"> {{lorem}} </p>
-  </form-helper>
+  <div>
+    <!-- con esto indicamos que usaremos un componente sin declararlo explicitamente-->
+    <!-- esto de arriba es lo mismo que hacer esto -->
+    <keep-alive><component v-bind:is="component"></component></keep-alive>
+    <button v-on:click="changeForm">Cambiar Componente</button>
+    <!-- <form-one></form-one> -->
+    <!-- parece que la principal idea de el uso de esta etiqueta es que podamos hacer uso de multiples componentes
+       dependiendo del resutado de condicionales o algo asi es que se me ocurre -->
+    <!-- podemos usar data binding en dichas etiquetas, gracias a esto podemos crear alguna propiedad que nos pueda
+         indicar en que momentos podriamos usar dichos componentes importados -->
+
+    <!-- al movernos entre componentes dinamicos lo que hacemos es destruir el componente previo, por ende perdiendo los datos
+    asociados o ingresados en dicho componente, para evitar esto podemos hacer uso de la etiqueta keep-alive la cual nos permite
+    salvar o guardar los datos que tenia almacenado dicho componente antes de cambiarse -->
+
   </div>
 </template>
 <script>
-import formHelper from "./components/FormHelper.vue";
-import formOne from "./components/FormOne.vue"
-import formTwo from "./components/FormTwo.vue"
+  import formHelper from "./components/FormHelper.vue";
+  import formOne from "./components/FormOne.vue"
+  import formTwo from "./components/FormTwo.vue"
 
-export default{
-  components: {
-    'form-helper': formHelper,
-    'form-one': formOne,
-    'form-two': formTwo
-  },
-  data(){
-    return{
-      titulo: 'Texto insertado en el componente formHelper',
-      lorem:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aliquam similique consectetur hic laborum tempore sapiente cum et delectus iure magnam est doloremque doloribus numquam blanditiis ducimus, debitis odit soluta.'
+  export default {
+    components: {
+      'form-helper': formHelper,
+      'form-one': formOne,
+      'form-two': formTwo
+    },
+    data() {
+      return {
+        titulo: 'Texto insertado en el componente formHelper',
+        lorem: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aliquam similique consectetur hic laborum tempore sapiente cum et delectus iure magnam est doloremque doloribus numquam blanditiis ducimus, debitis odit soluta.',
+        component: 'form-one'
+      }
+    },
+    methods: {
+      changeForm: function () {
+        if (this.component == "form-one") {
+          this.component = 'form-two'
+        } else {
+          this.component = 'form-one'
+        }
+      }
     }
-  },
-  methods:{
-    
   }
-}
-
 </script>
 <style>
 </style>
--- src/components/FormHelper.vue
+++ src/components/FormHelper.vue
@@ -1,30 +1,65 @@
 <template>
     <div>
-        <slot name="titulo"></slot>
-        <h3>i am the form helper</h3>
-        <!-- la etiqueta slot nos permite ingresar contenido en el componente sin necesidad de generarlo directamente aca
-        es decir, en cualquier otro componente que llame a este el podra insertar contenido y este se renderizara como si fuera parte de este componente -->
-        <slot name="texto"></slot>
-        <!-- al insertar n slots esto lo que hace es repetir el contenido ingresado en el otro componente ya que vuejs no sabe en cual slot ubicar dicho contenido -->
-        <!-- para evitar esto podemos asignarle a los slot un nombre con el cual se podra identificar su ubicacion -->
+    <h1>Please fill out our form...</h1>
+        <form>
+            <div id="form-header">
+                <slot name="form-header"></slot>
+            </div>
+            <div id="form-fields">
+                <slot name="form-fields"></slot>
+            </div>
+            <div id="form-controls">
+                <slot name="form-controls"></slot>
+            </div>
+            <div id="useful-links">
+                <ul>
+                    <li><a href="#">link 1</a></li>
+                    <li><a href="#">link 2</a></li>
+                    <li><a href="#">link 3</a></li>
+                    <li><a href="#">link 4</a></li>
+                </ul>
+            </div>
+        </form>
     </div>
 </template>
-<script>
-export default {
-    data() {
-        return{
 
+<script>
+// Imports
+export default {
+    components: {
+    },
+    data () {
+        return {
         }
     },
-    components:{
-
-    }, 
-    methods:{
-        
+    methods: {
     }
 }
 </script>
 
 <style scoped>
-/* se pueden agregar estilos a los slot  */
+h1{
+    text-align: center;
+}
+form{
+    width: 100%;
+    max-width: 960px;
+    margin: 0 auto;
+}
+#useful-links ul{
+    padding: 0;
+}
+#useful-links li{
+    display: inline-block;
+    margin-right: 10px;
+}
+form > div{
+    padding: 20px;
+    background: #eee;
+    margin: 20px 0;
+}
+#form-header{
+    background: #ddd;
+    border: 1px solid #bbb;
+}
 </style>
