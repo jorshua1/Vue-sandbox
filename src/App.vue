e259e048898a36923762b2f1026f3aeec461b474
Jorshua Hernandez <jorshua321@gmail.com>
2021-12-22 17:34:03 -0500

    se agregan objetos que hacen a referencia al checkbox biding o como pasar valores desde un checkbox a un arreglo

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,36 @@
 <template>
-  <div>
-    <!-- con esto indicamos que usaremos un componente sin declararlo explicitamente-->
-    <!-- esto de arriba es lo mismo que hacer esto -->
-    <keep-alive><component v-bind:is="component"></component></keep-alive>
-    <button v-on:click="changeForm">Cambiar Componente</button>
-    <!-- <form-one></form-one> -->
-    <!-- parece que la principal idea de el uso de esta etiqueta es que podamos hacer uso de multiples componentes
-       dependiendo del resutado de condicionales o algo asi es que se me ocurre -->
-    <!-- podemos usar data binding en dichas etiquetas, gracias a esto podemos crear alguna propiedad que nos pueda
-         indicar en que momentos podriamos usar dichos componentes importados -->
-
-    <!-- al movernos entre componentes dinamicos lo que hacemos es destruir el componente previo, por ende perdiendo los datos
-    asociados o ingresados en dicho componente, para evitar esto podemos hacer uso de la etiqueta keep-alive la cual nos permite
-    salvar o guardar los datos que tenia almacenado dicho componente antes de cambiarse -->
-
-  </div>
+<div id="App">
+  <add-blog></add-blog>
+</div>
 </template>
 <script>
-  import formHelper from "./components/FormHelper.vue";
-  import formOne from "./components/FormOne.vue"
-  import formTwo from "./components/FormTwo.vue"
+import addBlog from './components/addBlog'
 
-  export default {
-    components: {
-      'form-helper': formHelper,
-      'form-one': formOne,
-      'form-two': formTwo
-    },
-    data() {
-      return {
-        titulo: 'Texto insertado en el componente formHelper',
-        lorem: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aliquam similique consectetur hic laborum tempore sapiente cum et delectus iure magnam est doloremque doloribus numquam blanditiis ducimus, debitis odit soluta.',
-        component: 'form-one'
-      }
-    },
-    methods: {
-      changeForm: function () {
-        if (this.component == "form-one") {
-          this.component = 'form-two'
-        } else {
-          this.component = 'form-one'
-        }
-      }
+export default ({
+  components: {
+    'add-blog': addBlog
+  },
+  data() {
+    return {
+
     }
+  },
+  methods: {
+
   }
+})
 </script>
-<style>
+
+<style >
+body{
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+#App{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
 </style>
---
+++ src/components/addBlog.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="add-blog">
+    <h2>Add a new blog post</h2>
+    <form>
+      <label>Blog Title: </label>
+      <input type="text" v-model="blog.title" required>
+      <label for="">Blog content: </label>
+      <textarea v-model="blog.content"></textarea>
+    <div id="checkboxes">
+      <label >Uno</label>
+      <input type="checkbox" value="Uno" v-model="blog.categories">
+      <label >Dos</label>
+      <input type="checkbox" value="Dos" v-model="blog.categories">
+      <label >Tres</label>
+      <input type="checkbox" value="Tres" v-model="blog.categories">
+      <label >Cuatro</label>
+      <input type="checkbox" value="Cuatro" v-model="blog.categories">
+    </div>
+    </form>
+    <div id="preview">
+      <h3>Preview Blog</h3>
+      <p>Blog Title:  {{blog.title}} </p>
+      <p>Blog Content:  {{blog.content}} </p>
+      <p>Blog Categories</p>
+      <ul>
+        <li v-for="category in blog.categories">{{category}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+  // import { defineComponent } from '@vue/composition-api'
+
+  export default ({
+    data() {
+      return {
+        blog: {
+          title:'',
+          content:'',
+          categories:[]
+        }
+      }
+    },
+    methods: {
+
+    }
+  })
+</script>
+<style scoped>
+form{
+  width: 40vw;
+  display: flex;
+  flex-direction: column;
+}
+label{
+  font-weight: bold;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+input{
+  margin-top: 10px;
+  height: 30px;
+  border:1px solid lightgray;
+}
+
+input:hover{
+  border:1px solid lightgreen;
+}
+
+textarea{
+  /* background-color: whitesmoke; */
+  border:1px solid lightgray;
+
+}
+
+#preview{
+  margin-top: 20px;
+  border: 1.5px dotted lightgray;
+  padding: 20px;
+}
+
+#checkboxes input{
+  display: inline-block;
+  margin-right: 10px;
+}
+
+#checkboxes label{
+  display: inline-block;
+  /* margin: 10px; */
+}
+</style>
